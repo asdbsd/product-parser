567d68a6c8a7aaea4a2aab1c3ccc7a3b977e8085
Dobriyan Petkov <dobriyanp@yahoo.com>
2022-05-06 20:58:36 +0100

    completed refactoring file

--- productParser.js
+++ productParser.js
@@ -1,62 +1,78 @@
 const fs = require('fs/promises');
 const inputArguments = process.argv;
-const acceptedFileTypes = ['csv']
+const acceptedFileTypes = ['csv'];
 
 
+let fileType = '';
 const commands = {};
+const inputIsValid = checkInput(inputArguments);
 
+const actions = {
+    "csv": parseCSV.bind(null, inputArguments)
+}
 
+if(inputIsValid && actions[fileType]) {
+    actions[fileType]();
+}
 
-
-
-parseCSV(inputArguments);
-
-async function parseCSV(consoleInputArgs) {
-
+function checkInput(consoleInputArgs) {
     if (consoleInputArgs.length < 3) {
         console.log('Please provide arguments in order to use this service!');
-        console.log('Usage: "node productParser --file FILE_LOCATION --unique-combinations=NEW_FILE_NAME"')
-        console.log('Select file to parse: ')
-        return;
-    }
+        console.log('Usage: "node productParser --file FILE_LOCATION --unique-combinations=NEW_FILE_NAME"');
+        console.log('Select file to parse: ');
+        return false;
+    } else {
+        for (let i = 2; i < consoleInputArgs.length; i++) {
+            if (consoleInputArgs[i].startsWith('--')) {
+                const command = consoleInputArgs[i].slice(2);
+                if (command.split('=').length > 1) {
+                    let [currentCommand, currentValue] = [...command.split('=')];
+                    fileType = currentValue.split('.')[1];
 
-    for (let i = 2; i < consoleInputArgs.length; i++) {
-        if (consoleInputArgs[i].startsWith('--')) {
-            const command = consoleInputArgs[i].slice(2);
-            if (command.split('=').length > 1) {
-                let [currentCommand, currentValue] = [...command.split('=')]
-                let fileType = currentValue.split('.')[1];
+                    if (!acceptedFileTypes.includes(fileType)) {
+                        console.log(`Output file type of ${fileType} is not supported`);
+                        return false;
+                    }
 
-                if (!acceptedFileTypes.includes(fileType)) {
-                    console.log(`Output file type of ${fileType} is not supported`);
-
+                    commands[currentCommand] = currentValue;
+                    return true;
                 }
 
-                commands[currentCommand] = currentValue
-                continue;
-            }
+                if (!consoleInputArgs[i + 1]) {
+                    console.log(`Error: After command: ${command} you must include a value.`);
+                    return false;
+                }
+                
+                fileType = consoleInputArgs[i + 1].split('.')[1];
+                if (!acceptedFileTypes.includes(fileType)) {
+                    console.log(`Input file type of ${fileType} is not supported`);
+                    return false;
+                }
 
-            if (!consoleInputArgs[i + 1]) {
-                console.log(`Error: After command: ${command} you must include a value.`)
-                return;
+                commands[`${command}`] = consoleInputArgs[i + 1];
+                i += 1;
             }
-            const fileType = consoleInputArgs[i + 1].split('.')[1];
-
-            if (!acceptedFileTypes.includes(fileType)) {
-                console.log(`Input file type of ${fileType} is not supported`);
-                return;
-            }
-
-            commands[`${command}`] = consoleInputArgs[i + 1];
-            i += 1;
         }
-    }
 
-    if (!commands['file'] || !commands['unique-combinations']) {
-        console.log('You must use both: file and unique-combinations commands in order to use this service.');
-        return;
+        if (!commands['file'] || !commands['unique-combinations']) {
+            console.log('You must use both: file and unique-combinations commands in order to use this service.');
+            return false;
+        }
+        return true;
     }
+}
 
+
+
+async function parseCSV() {
+
+    const stats = await fs.stat(`${commands['file']}`);
+    const fileSizeInMb = Math.round(stats.size / (1024 * 1024));
+    
+    if (fileSizeInMb > 20) {
+        console.log('File can\'t be larger than 15 MB');
+        return false;
+    }
 
     let filteredRows = [];
     let headings = '';
@@ -69,14 +85,14 @@ async function parseCSV(consoleInputArgs) {
     const separatedRows = text.split("\n").map(item => item.replace(/"/g, ''));
 
     headings = separatedRows.splice(0, 1)[0].split(',').map(item => item.split('_')[0]);
-    headings.push('count\n')
+    headings.push('count\n');
     headings = headings.join(',');
     console.log('Parsing...');
     filteredRows = separatedRows.reduce((acc, currentRow, i) => {
 
         const [make, model, condition, capacity, grade, colour, network] = [...currentRow.split(',')];
         if (!make || !model) {
-            console.log('Make and Model are required in every product.')
+            console.log('Make and Model are required in every product.');
             return acc;
         }
 
@@ -116,11 +132,10 @@ async function parseCSV(consoleInputArgs) {
     }, []);
 
 
-    console.log(`Saving to new ${commands['unique-combinations']}`, );
+    console.log(`Saving to new ${commands['unique-combinations']}`,);
     await fs.writeFile(commands['unique-combinations'], (headings));
-    
-    for(let row of filteredRows) {
 
+    for (let row of filteredRows) {
         await fs.appendFile(commands['unique-combinations'], row);
     }
     console.log('Completed!');
