1eaaadc86c774ddab4f6364438b90d3445395d3a
Dobriyan Petkov <dobriyanp@yahoo.com>
2022-05-06 19:08:18 +0100

    completed file reading, filtering and adding new file with new data

--- productParser.js
+++ productParser.js
@@ -1,2 +1,127 @@
-const fileSystem = required('fs');
+const fs = require('fs/promises');
+const inputArguments = process.argv;
+const acceptedFileTypes = ['csv']
 
+
+const commands = {};
+
+
+
+
+
+parseCSV(inputArguments);
+
+async function parseCSV(consoleInputArgs) {
+
+    if (consoleInputArgs.length < 3) {
+        console.log('Please provide arguments in order to use this service!');
+        console.log('Usage: "node productParser --file FILE_LOCATION --unique-combinations=NEW_FILE_NAME"')
+        console.log('Select file to parse: ')
+        return;
+    }
+
+    for (let i = 2; i < consoleInputArgs.length; i++) {
+        if (consoleInputArgs[i].startsWith('--')) {
+            const command = consoleInputArgs[i].slice(2);
+            if (command.split('=').length > 1) {
+                let [currentCommand, currentValue] = [...command.split('=')]
+                let fileType = currentValue.split('.')[1];
+
+                if (!acceptedFileTypes.includes(fileType)) {
+                    console.log(`Output file type of ${fileType} is not supported`);
+
+                }
+
+                commands[currentCommand] = currentValue
+                continue;
+            }
+
+            if (!consoleInputArgs[i + 1]) {
+                console.log(`Error: After command: ${command} you must include a value.`)
+                return;
+            }
+            const fileType = consoleInputArgs[i + 1].split('.')[1];
+
+            if (!acceptedFileTypes.includes(fileType)) {
+                console.log(`Input file type of ${fileType} is not supported`);
+                return;
+            }
+
+            commands[`${command}`] = consoleInputArgs[i + 1];
+            i += 1;
+        }
+    }
+
+    if (!commands['file'] || !commands['unique-combinations']) {
+        console.log('You must use both: file and unique-combinations commands in order to use this service.');
+        return;
+    }
+
+
+    let filteredRows = [];
+    let headings = '';
+
+    const fileHandler = await fs.open(`${commands['file']}`);
+    const data = await fileHandler.readFile();
+    fileHandler.close();
+
+    const text = data.toString();
+    const separatedRows = text.split("\n").map(item => item.replace(/"/g, ''));
+
+    headings = separatedRows.splice(0, 1)[0].split(',').map(item => item.split('_')[0]);
+    headings.push('count\n')
+    headings = headings.join(',');
+    console.log('Parsing...');
+    filteredRows = separatedRows.reduce((acc, currentRow, i) => {
+
+        const [make, model, condition, capacity, grade, colour, network] = [...currentRow.split(',')];
+        if (!make || !model) {
+            console.log('Make and Model are required in every product.')
+            return acc;
+        }
+
+        if (acc.length) {
+            const filteredProduct = acc.filter(product => product.make === make
+                && product.model === model
+                && product.colour === colour
+                && product.capacity === capacity
+                && product.grade === grade
+                && product.condition === condition
+                && product.network === network)[0];
+
+            if (filteredProduct) {
+                filteredProduct.count += 1;
+                return acc;
+            }
+        }
+
+        acc.push({ make, model, condition: condition || '', capacity: capacity || '', grade: grade || '', colour: colour || '', network: network || '', count: 1 });
+
+        return acc;
+    }, []);
+
+    filteredRows = filteredRows.reduce((acc, currentObj, index) => {
+        console.log(`${index + 1}. 
+        Make: ${currentObj.make}, 
+        Model: ${currentObj.model}, 
+        Condition: ${currentObj.condition}, 
+        Capacity: ${currentObj.capacity}, 
+        Grade: ${currentObj.grade}, 
+        Color: ${currentObj.colour}, 
+        Network: ${currentObj.network},
+        Count: ${currentObj.count}`);
+
+        acc.push(`${currentObj.make},${currentObj.model},${currentObj.condition},${currentObj.capacity},${currentObj.grade},${currentObj.colour},${currentObj.network},${currentObj.count}\n`);
+        return acc;
+    }, []);
+
+
+    console.log(`Saving to new ${commands['unique-combinations']}`, );
+    await fs.writeFile(commands['unique-combinations'], (headings));
+    
+    for(let row of filteredRows) {
+
+        await fs.appendFile(commands['unique-combinations'], row);
+    }
+    console.log('Completed!');
+}
